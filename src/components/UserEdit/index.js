fe7983d0489e649d1062563a80abe0f65db4c0d7
Diego Varela <diego.varela@corp.globo.com>
2020-06-28 19:36:32 -0300

    [UserEdit] Exibe o form apenas apos o fetch

--- src/components/UserEdit/index.js
+++ src/components/UserEdit/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const UserEdit = () => {
   const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const getAPI = async () => {
@@ -10,32 +11,39 @@ const UserEdit = () => {
         'http://localhost:8000/users/1/',
       );
       setData(result.data)
+      setIsLoading(false)
     }
 
     getAPI()
   }, [])
 
   return (
-
-    <form>
-    <div class="form-group">
-      <label for="userEmail">Endereço de email</label>
-      <input type="email" readonly class="form-control-plaintext" id="userEmail" aria-describedby="emailHelp" value={data.email}/>
-    </div>
-    <div class="form-group">
-      <label for="userName">Nome</label>
-      <input type="text" class="form-control" id="userName"/>
-    </div>
-    <div class="form-group">
-      <label for="userPassword">Senha</label>
-      <input type="password" class="form-control" id="userPassword"/>
-    </div>
-    <div class="form-group form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-      <label class="form-check-label" for="exampleCheck1">Is Staff</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Enviar</button>
-  </form>
+    <React.Fragment>
+    { isLoading
+      ? <div>Loading...</div>
+      : (
+        <form>
+          <div class="form-group">
+            <label for="userEmail">Endereço de email</label>
+            <input type="email" readonly class="form-control-plaintext" id="userEmail" aria-describedby="emailHelp" value={data.email}/>
+          </div>
+          <div class="form-group">
+            <label for="userName">Nome</label>
+            <input type="text" class="form-control" id="userName"/>
+          </div>
+          <div class="form-group">
+            <label for="userPassword">Senha</label>
+            <input type="password" class="form-control" id="userPassword"/>
+          </div>
+          <div class="form-group form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+            <label class="form-check-label" for="exampleCheck1">Is Staff</label>
+          </div>
+          <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+      )
+    }
+    </React.Fragment>
   )
 }
 
