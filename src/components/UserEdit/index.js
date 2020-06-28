11a581d3084da1993c85b75fc2979ac459b14e8d
Diego Varela <diego.varela@corp.globo.com>
2020-06-28 19:26:35 -0300

    [UserEdit] Buscando dados do usuario na API

--- src/components/UserEdit/index.js
+++ src/components/UserEdit/index.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const UserEdit = ({email, name, password}) => (
-  <form>
+const UserEdit = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const getAPI = async () => {
+      const result = await axios(
+        'http://localhost:8000/users/1/',
+      );
+      setData(result.data)
+    }
+
+    getAPI()
+  }, [])
+
+  return (
+
+    <form>
     <div class="form-group">
       <label for="userEmail">Endereço de email</label>
-      <input type="email" readonly class="form-control-plaintext" id="userEmail" aria-describedby="emailHelp" value={email}/>
+      <input type="email" readonly class="form-control-plaintext" id="userEmail" aria-describedby="emailHelp" value={data.email}/>
     </div>
     <div class="form-group">
       <label for="userName">Nome</label>
@@ -20,6 +36,7 @@ const UserEdit = ({email, name, password}) => (
     </div>
     <button type="submit" class="btn btn-primary">Enviar</button>
   </form>
-)
+  )
+}
 
 export default UserEdit
