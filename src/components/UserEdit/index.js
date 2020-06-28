f72ce14f34e695980513d52c327637ca333bb172
Diego Varela <diego.varela@corp.globo.com>
2020-06-28 19:51:58 -0300

    [UserEdit] Criando fluxo de erro no fetch do usuario

--- src/components/UserEdit/index.js
+++ src/components/UserEdit/index.js
@@ -10,14 +10,18 @@ const UserEdit = () => {
 
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true)
+  const [hasError, setHasError] = useState(false)
 
   useEffect(() => {
     const getAPI = async () => {
-      const result = await axios(
-        `http://localhost:8000/users/${id}/`,
-      );
-      setData(result.data)
-      setIsLoading(false)
+      try{
+        const result = await axios(`http://localhost:8000/users/${id}/`);
+        setData(result.data)
+      } catch {
+        setHasError(true)
+      } finally {
+        setIsLoading(false)
+      }
     }
 
     getAPI()
@@ -27,27 +31,29 @@ const UserEdit = () => {
     <React.Fragment>
     { isLoading
       ? <div>Loading...</div>
-      : (
-        <form>
-          <div className="form-group">
-            <label htmlFor="userEmail">Endereço de email</label>
-            <input type="email" readOnly className="form-control-plaintext" id="userEmail" aria-describedby="emailHelp" value={data.email}/>
-          </div>
-          <div className="form-group">
-            <label htmlFor="userName">Nome</label>
-            <input type="text" className="form-control" id="userName"/>
-          </div>
-          <div className="form-group">
-            <label htmlFor="userPassword">Senha</label>
-            <input type="password" className="form-control" id="userPassword"/>
-          </div>
-          <div className="form-group form-check">
-            <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-            <label className="form-check-label" htmlFor="exampleCheck1">Is Staff</label>
-          </div>
-          <button type="submit" className="btn btn-primary">Enviar</button>
-        </form>
-      )
+      : hasError
+        ? <div>Não foi possível encontrar o usuário...</div>
+        : (
+          <form>
+            <div className="form-group">
+              <label htmlFor="userEmail">Endereço de email</label>
+              <input type="email" readOnly className="form-control-plaintext" id="userEmail" aria-describedby="emailHelp" value={data.email}/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="userName">Nome</label>
+              <input type="text" className="form-control" id="userName"/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="userPassword">Senha</label>
+              <input type="password" className="form-control" id="userPassword"/>
+            </div>
+            <div className="form-group form-check">
+              <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+              <label className="form-check-label" htmlFor="exampleCheck1">Is Staff</label>
+            </div>
+            <button type="submit" className="btn btn-primary">Enviar</button>
+          </form>
+        )
     }
     </React.Fragment>
   )
