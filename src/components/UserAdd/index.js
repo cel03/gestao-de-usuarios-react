a3f6d2cc997e7374466a651dcb0ec45ccf4c598e
Diego Varela <diego.varela@corp.globo.com>
2020-06-29 00:59:50 -0300

    [UserAdd][AlertSucess] Refactor para aviso de usuario criado com sucesso

--- src/components/UserAdd/index.js
+++ src/components/UserAdd/index.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Loading from "../Loading"
+import AlertSuccess from "../UI/AlertSuccess"
 
 const UserAdd = () => {
 
   const [isLoading, setIsLoading] = useState(false)
   const [hasError, setHasError] = useState(false)
   const [user, setUser] = useState({})
+  const [userAdded, setUserAdded] = useState(false)
 
   const handleInputChange = (event) => {
     const target = event.target;
@@ -16,9 +18,12 @@ const UserAdd = () => {
     setUser({ ...user, [name]: value })
   }
 
-  const addUser = () => {
-    console.log({...user})
-    axios.post(`http://localhost:8000/users/`, {...user})
+  const addUser = async () => {
+    try{
+      await axios.post(`http://localhost:8000/users/`, {...user})
+      setUserAdded(true)
+    } catch {
+    }
   }
 
   return (
@@ -27,30 +32,31 @@ const UserAdd = () => {
       ? <Loading />
       : hasError
         ? <div>Não foi possível encontrar o usuário...</div>
-        : (
-          <form onSubmit={(event)=> {
-            event.preventDefault()
-            addUser()
-          }}>
-            <div className="form-group">
-              <label htmlFor="userEmail">Endereço de email</label>
-              <input type="email" className="form-control" id="userEmail" aria-describedby="emailHelp" name="email" value={user.email} onChange={handleInputChange} required/>
-            </div>
-            <div className="form-group">
-              <label htmlFor="userName">Nome</label>
-              <input type="text" className="form-control" id="userName" name="name" value={user.name} onChange={handleInputChange} required/>
-            </div>
-            <div className="form-group">
-              <label htmlFor="userPassword">Senha</label>
-              <input type="password" className="form-control" id="userPassword" name="email"/>
-            </div>
-            <div className="form-group form-check">
-              <input type="checkbox" className="form-check-input" id="exampleCheck1" name="is_staff" checked={user.is_staff} onChange={handleInputChange}/>
-              <label className="form-check-label" htmlFor="exampleCheck1">Is Staff</label>
-            </div>
-            <button type="submit" className="btn btn-primary">Enviar</button>
-          </form>
-        )
+        : <React.Fragment>
+            <AlertSuccess show={userAdded}/>
+            <form onSubmit={(event)=> {
+              event.preventDefault()
+              addUser()
+            }}>
+              <div className="form-group">
+                <label htmlFor="userEmail">Endereço de email</label>
+                <input type="email" className="form-control" id="userEmail" aria-describedby="emailHelp" name="email" value={user.email} onChange={handleInputChange} required/>
+              </div>
+              <div className="form-group">
+                <label htmlFor="userName">Nome</label>
+                <input type="text" className="form-control" id="userName" name="name" value={user.name} onChange={handleInputChange} required/>
+              </div>
+              <div className="form-group">
+                <label htmlFor="userPassword">Senha</label>
+                <input type="password" className="form-control" id="userPassword" name="email"/>
+              </div>
+              <div className="form-group form-check">
+                <input type="checkbox" className="form-check-input" id="exampleCheck1" name="is_staff" checked={user.is_staff} onChange={handleInputChange}/>
+                <label className="form-check-label" htmlFor="exampleCheck1">Is Staff</label>
+              </div>
+              <button type="submit" className="btn btn-primary">Enviar</button>
+            </form>
+          </React.Fragment>
     }
     </React.Fragment>
   )
