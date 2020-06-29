b94877a1f7d59d5c327819207b227db6361efa1f
Diego Varela <diego.varela@corp.globo.com>
2020-06-29 00:59:02 -0300

    [AlertSucess] novo componente

---
+++ src/components/UI/AlertSuccess.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const AlertSuccess = ({userAdded}) => {
+  return (
+    <React.Fragment>
+    {
+      userAdded
+      ?(
+        <div className="alert alert-success alert-dismissible fade show" role="alert">
+          Usuário <strong>criado</strong> com sucesso!
+          <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+      )
+      : null
+  }
+  </React.Fragment>
+  )
+}
+
+export default AlertSuccess
