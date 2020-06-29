3cc2a45a66be98db72c3b0c0de51a49f823911fd
Diego Varela <diego.varela@corp.globo.com>
2020-06-29 16:55:11 -0300

    [refactor] class -> className

--- src/components/UI/ActionButtons.js
+++ src/components/UI/ActionButtons.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Link } from "react-router-dom";
 
 const ActionButtons = ({userId, callbackDeleteUserClick}) => (
-  <div class="btn-group" role="group" aria-label="Basic example">
-    <button type="button" class="btn btn-secondary"> <Link to={`/user/${userId}`} style={{color: 'inherit'}}>Editar</Link></button>
-    <button type="button" class="btn btn-secondary" onClick={()=>{callbackDeleteUserClick(userId)}}>Deletar</button>
+  <div className="btn-group" role="group" aria-label="Basic example">
+    <button type="button" classNamelass="btn btn-secondary"> <Link to={`/user/${userId}`} style={{color: 'inherit'}}>Editar</Link></button>
+    <button type="button" className="btn btn-secondary" onClick={()=>{callbackDeleteUserClick(userId)}}>Deletar</button>
   </div>
 )
 
