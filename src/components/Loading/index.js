797ba81887d8b6a87e06b5937970a6aaf3aeb779
Diego Varela <diego.varela@corp.globo.com>
2020-06-28 21:30:17 -0300

    [Loading][estilo] alinhamento ao centro

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
 const Loading = () => (
-  <div class="spinner-grow" role="status">
-    <span class="sr-only">Loading...</span>
+  <div class="d-flex justify-content-center">
+    <div class="spinner-border" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
   </div>
 )
 
