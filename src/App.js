ea449ce71f9002e5b8957c95feda5a51375e7513
Diego Varela <diego.varela@corp.globo.com>
2020-06-28 21:06:54 -0300

    [App.js] Uso da Navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
 import UserList from './components/UserList'
 import UserEdit from './components/UserEdit'
+import NavBar from './components/NavBar'
 
 function App() {
   const users = [
@@ -15,14 +16,17 @@ function App() {
     }
   ]
   return (
-    <Switch>
-      <Route exact path="/">
-        <UserList users={users} />
-      </Route>
-      <Route path="/user/:id">
-        <UserEdit />
-      </Route>
-    </Switch>
+    <div class="container.fluid">
+      <NavBar/>
+      <Switch>
+        <Route exact path="/">
+          <UserList users={users} />
+        </Route>
+        <Route path="/user/:id">
+          <UserEdit />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
