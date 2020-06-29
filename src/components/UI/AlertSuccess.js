009614b9c510944fd21bb57101473f143d8263eb
Diego Varela <diego.varela@corp.globo.com>
2020-06-29 16:26:31 -0300

    [AlertSucess] Apenas renderização

--- src/components/UI/AlertSuccess.js
+++ src/components/UI/AlertSuccess.js
@@ -1,21 +1,13 @@
 import React from 'react'
 
-const AlertSuccess = ({show}) => {
+const AlertSuccess = ({message}) => {
   return (
-    <React.Fragment>
-    {
-      show
-      ?(
-        <div className="alert alert-success alert-dismissible fade show" role="alert">
-          Usuário <strong>criado</strong> com sucesso!
-          <button type="button" className="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-      )
-      : null
-  }
-  </React.Fragment>
+  <div className="alert alert-success alert-dismissible fade show" role="alert">
+    {message}
+    <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
   )
 }
 
